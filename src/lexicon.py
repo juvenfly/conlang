69c9c792ad89ae00b97c5308ab6a0cb422dd249b
Adams LaBorde <juvenfly@gmail.com>
2018-12-12 12:40:29 -0600

    stub out lexicon class

--- src/lexicon.py
+++ src/lexicon.py
@@ -1,4 +1,13 @@
 
+class Lexicon(object):
+    def __init__(self, phonology, word_list):
+        self.phonology = phonology
+        self.word_list = set()
+
+    def generate_lexicon(self):
+        raise NotImplementedError
+
+
 class Word(object):
     def __init__(self):
         self.underlying_representation = None
