d436a5fbef352a582c9580c9000011eb613330d5
Adams LaBorde <juvenfly@gmail.com>
2018-12-17 20:40:54 -0600

    where did those semicolons come from?

--- sample.py
+++ sample.py
@@ -11,7 +11,7 @@ def main():
     phonology = build_phonology(phonemes)
 
     lexicon = Lexicon(phonology)
-    lexicon.generate_lexicon();;
+    lexicon.generate_lexicon()
 
 
 def build_phonemes():
